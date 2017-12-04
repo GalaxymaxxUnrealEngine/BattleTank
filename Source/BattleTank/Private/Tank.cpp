1fa75cfe7ba3c02822ef2cac14bc5d2f5d379ef8
Ruan Xun <galaxymaxx@outlook.com>
2017-12-04 01:39:13 -0500

    Adding a Quit Button

--- Source/BattleTank/Private/Tank.cpp
+++ Source/BattleTank/Private/Tank.cpp
@@ -48,7 +48,7 @@ void ATank::SetTurretReference(UTankTurret* TurretToSet)
 
 void ATank::Fire()
 {
-	float isReloaded = FPlatformTime::Seconds() - LastFireTime > ReloadTimeInSeconds;
+	double isReloaded = FPlatformTime::Seconds() - LastFireTime > ReloadTimeInSeconds;
 	if(isReloaded&&Barrel)
 	{
 		LastFireTime = FPlatformTime::Seconds();
